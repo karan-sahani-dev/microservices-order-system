d5212e0180f6547d9a416b5ff27916a9efc36e7e
karan-sahani-dev <karansahani1919@gmail.com>
2026-09-19 15:20:24 +0530

    Day 1: Bootstrap three independent microservices

---
+++ notification-service/src/test/java/com/karan/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karan.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
